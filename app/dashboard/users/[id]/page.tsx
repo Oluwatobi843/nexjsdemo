783829ae3baa84478f9f2f471ce6f37d324e1b6f
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-04-22 13:08:55 +0100

    update

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,8 +1,9 @@
 
-const UserDetails = ({ params } : { params: Promise <{ id: string }>}) => {
+const UserDetails = async ({ params } : { params: Promise <{ id: string }>}) => {
+    const { id } = await params;
   return (
     <div>
-      
+      <h1>Showing details for user #{id} </h1>
     </div>
   )
 }
