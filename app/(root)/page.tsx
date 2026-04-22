713676a2b4b260146f5e3c78ee600917471c2987
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-04-22 14:58:42 +0100

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,13 @@
-import Hello from "./components/hello";
+import Hello from "../components/hello";
 
 const Home = () => {
-
-  console.log('What type of component am i')
+  console.log("What type of component am i");
   return (
     <main>
       <div className="text-5xl underline">Welcome to Nextjs</div>
       <Hello />
     </main>
   );
-}
+};
 
-export default Home
+export default Home;
