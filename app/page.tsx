b6bb7d8ce5f32dc6c6d28349be8bfcccfbbd5286
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-04-24 23:14:33 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 const Page = () => {
   return (
     <section>
-       <h1 className="text-center">The Hub for Every Dev <br/>Event You  Cant Miss</h1>
-       <p className="text-center mt-5 ">Hackathons, Meetups, and Conferences, All in One Place</p>
+       <h1 className="text-center">The Hub For Every Dev <br/>Event You  Cant Miss</h1>
+       <p className="text-center mt-5 ">Hackathons, Meetups, and Conferences, All In One Place</p>
     </section>
   );
 }
