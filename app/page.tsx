e6c9238130b74414acba2f35786a658dc03aade6
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-04-24 23:39:21 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import ExploreBtn from "@/components/ExploreBtn";
 
 const Page = () => {
   return (
     <section>
        <h1 className="text-center">The Hub For Every Dev <br/>Event You  Cant Miss</h1>
        <p className="text-center mt-5 ">Hackathons, Meetups, and Conferences, All In One Place</p>
+       
+       <ExploreBtn/>
     </section>
   );
 }
