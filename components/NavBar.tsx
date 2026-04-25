be9d367c3aaa50b9df9b6f28bf030938f4ae1385
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-04-25 23:54:17 +0100

    update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
 
 const NavBar = () => {
   return (
-  <header>
-    <nav></nav>
-  </header>
-  )
-}
+    <header>
+      <nav>
+        <Link href="/" className="logo">
+          <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
-export default NavBar
+export default NavBar;
