57d1565031ba10b808745102f577748af5d3712e
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-04-26 17:19:14 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,14 @@ import ExploreBtn from "@/components/ExploreBtn";
 import { title } from "process";
 
 const events = [
-  { image: '/images/event1.png', title: 'Event 1'},
-  { image: '/images/event2.png', title: 'Event 2'},
+  { image: '/images/event1.png',
+     title: 'Event 1',
+     slug: 'event-1',
+     location: 'location-1',
+     date: 'Date-1',
+     time: 'Time-1'
+    },
+ 
 ]
 
 const Page = () => {
