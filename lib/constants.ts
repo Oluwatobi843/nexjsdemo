7b2b78c4fa477a53fd987001cf763de3179cb1ad
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-05-01 08:03:02 +0100

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@ export type EventItem = {
   image: string;
   title: string;
   slug: string;
-  location:  string;
+  location: string;
   date: string; // e.g., "2025-11-07"
   time: string; // e.g., "09:00 AM"
 };  
