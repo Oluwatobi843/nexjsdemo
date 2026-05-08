60bb789736d1aa671262a84b8ce577ed3b435818
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-05-08 17:33:34 +0100

    update

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -2,6 +2,7 @@ import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import { v2 as cloudinary } from "cloudinary";
 import Event from "@/database/event.model";
+import { error } from "console";
 
 // ✅ Uses CLOUDINARY_URL automatically from .env.local
 cloudinary.config({
@@ -106,9 +107,18 @@ export async function POST(req: NextRequest) {
 }
 
 export async function GET(){
+
   try {
-    
+
+    await connectDB();
+
+    const events = await Event.find().sort({ createdAt: -1});
+
+    return NextResponse.json({ message: 'Event fetched successfully', events}, {status: 200})
+
   } catch (e) {
-    
+      NextResponse.json({
+        message: 'Event fetching failed', error: e
+      }, {status: 500})
   }
 }
